e83486ff977904bf903a3f2a137370cd6ff6b1b2
AlanRENAULT35 <r.alandu35@gmail.com>
2024-04-25 06:29:49 -0400

    Dropdown success

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,26 +2,33 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
-  const {dispatch } = useContext(AppContext);
-
-    const changeLocation = (val)=>{
-            dispatch({
-                type: 'CHG_LOCATION',
-                payload: val,
-            })
+    const { dispatch } = useContext(AppContext);
+    const changeLocation = (val) => {
+        dispatch({
+            type: 'CHG_LOCATION',
+            payload: val,
+        })
     }
-    
+    // Les options du dropdown
+    const options = [
+        { value: '£', label: 'Uk(£)' },
+        { value: '₹', label: 'India(₹)' },
+        { value: '€', label: 'Europe(€)' },
+        { value: 'CAD', label: 'Canada(CAD)' }
 
-  return (
-        <div className='alert alert-secondary'> Location {
-      <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
-        <option value="£">Uk(£)</option>
-        <option value="₹">India(₹)</option>
-        <option value="€">Europe(€)</option>
-        <option value="CAD">Canada(CAD)</option>
-      </select>	
-      }	
-    </div>
+    ];
+    const customStyle = {
+        backgroundColor: 'lightgreen', // Vert foncé
+        color: 'white',
+    };
+    return (
+        <select name="Location" id="Location" onChange={event => changeLocation(event.target.value)} className='form-select' style={customStyle}>
+            {options.map((option) => (
+                <option key={option.value} value={option.value} style={customStyle}>
+                    {option.label}
+                </option>
+            ))}
+        </select>
     );
 };
 
